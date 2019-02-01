5f4d84d56b00ad65498270b75407ee856b072f59
tigran94 <tiktor94@gmail.com>
2019-02-01 12:12:13 +0400

    Fix /profile/id url

--- clientServer/src/main/java/clientSide/controllers/ProfileController.java
+++ clientServer/src/main/java/clientSide/controllers/ProfileController.java
@@ -1,5 +1,6 @@
 package clientSide.controllers;
 
+import clientSide.dto.JobTitle;
 import clientSide.services.PostDao;
 import clientSide.entities.Post;
 import org.springframework.security.core.Authentication;
@@ -7,6 +8,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 @Controller
 @RequestMapping("/profile")
@@ -43,6 +46,20 @@ public class ProfileController {
         return "redirect:/";
     }
 
+    @RequestMapping(value = "/{jobId}", method = RequestMethod.GET)
+    public ModelAndView getJobById(@PathVariable("jobId") long id, HttpServletRequest req) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        ModelAndView modelAndView = new ModelAndView("profile");
+        List<JobTitle> jobTitles;
+        if( req.getSession().getAttribute("jobTitles")==null){
+            jobTitles= postDao.getJobTitles(authentication.getName());
+        }else {
+            jobTitles = (List<JobTitle>) req.getSession().getAttribute("jobTitles");
+        }
+        modelAndView.addObject("jobTitles", jobTitles);
+        modelAndView.addObject("post", postDao.getJobAnnouncementByIdWithStream(id));
+        return modelAndView;
+    }
 
     @RequestMapping(method = RequestMethod.POST)
     public ModelAndView homeString(HttpServletRequest req,
